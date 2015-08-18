04715e92c7fc514c877841e30f4bb4cb3dcc81e3
Raquel Hernandez <r@raquel.nyc>
2015-08-18 18:45:33 -0400

    trying to get add to work

--- gtf.go
+++ gtf.go
@@ -89,9 +89,34 @@ var GtfFuncMap = template.FuncMap{
 
 		return url.QueryEscape(s)
 	},
-	"add": func(x, y int) int {
+	"add": func(arg interface{}, value interface{}) bool {
 		defer recovery()
-		return x + y
+
+		var v float64
+		switch value.(type) {
+		case int, int8, int16, int32, int64:
+			v = float64(reflect.ValueOf(value).Int())
+		case uint, uint8, uint16, uint32, uint64:
+			v = float64(reflect.ValueOf(value).Uint())
+		case float32, float64:
+			v = reflect.ValueOf(value).Float()
+		default:
+			return false
+		}
+
+		var a float64
+		switch arg.(type) {
+		case int, int8, int16, int32, int64:
+			a = float64(reflect.ValueOf(arg).Int())
+		case uint, uint8, uint16, uint32, uint64:
+			a = float64(reflect.ValueOf(arg).Uint())
+		case float32, float64:
+			a = reflect.ValueOf(arg).Float()
+		default:
+			return false
+		}
+
+		return v + a
 	},
 	"wordcount": func(s string) int {
 		defer recovery()
